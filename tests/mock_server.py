fa633432a347edf156fb73827ea3f4c8ff9ef155
Shenghang Tsai <jackalcooper@gmail.com>
2026-01-13 16:59:52 +0800

    Update mock_server.py

--- tests/mock_server.py
+++ tests/mock_server.py
@@ -423,6 +423,9 @@ class DeepSeekProxy:
             else:
                 extra_body["top_k"] = params.top_k
 
+        if params.enable_thinking:
+            extra_body["enable_thinking"] = True
+
         if params.enable_thinking and params.thinking_budget is not None:
             extra_body["thinking_budget"] = params.thinking_budget
 
